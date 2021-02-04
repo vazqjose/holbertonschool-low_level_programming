ea2916e70d2b4fef2679bc542a1d3849423656f4
vazqjose <vazqjose@gmail.com>
2021-02-04 22:39:44 +0000

    File uploaded

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main - entry point
+ *
+ *Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	int current_no;
+	char current_ch;
+
+	current_no = '0';
+	current_ch = 'a';
+
+	while (current_no <= '9')
+	{
+		putchar(current_no);
+		current_no++;
+	}
+
+	while (current_no <= 'f')
+	{
+		putchar(current_ch);
+		current_ch++;
+	}
+
+	putchar('\n');
+	return (0);
+}
