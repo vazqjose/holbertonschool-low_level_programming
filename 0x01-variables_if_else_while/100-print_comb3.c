36d246220032b250639cd24505187b5f05e62c3c
vazqjose <vazqjose@gmail.com>
2021-02-05 23:06:21 +0000

    First attempt at code

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,6 +10,49 @@
 
 int main(void)
 {
-	putchar('\n');
-	return (0);
+	int digit1 = '0';
+	int digit2 = '0';
+
+	while (digit1 <= '9')
+	{
+
+		while (digit2 <= '9')
+		{
+			if (digit2 == digit1)
+			{
+				/*	do nothing	*/
+			}
+			else if (digit2 == '0')
+			{
+			}
+			else
+			{
+				putchar(digit1);
+				putchar(digit2);
+
+				if (digit1 == '9' && digit2 == '9')
+				{
+					digit1++;
+					digit2++;
+					break;
+				}
+
+				putchar(',');
+				putchar(' ');
+
+				if (digit2 != '9')
+				{
+					digit2++;
+				}
+				else
+				{
+					digit2 = '0';
+					digit1++;
+				}
+			}
+		}
+	}
+
+		putchar('\n');
+		return (0);
 }
