6eaedf559aac1669ff83e16ccca2ace99ef91c32
vazqjose <vazqjose@gmail.com>
2021-02-09 23:33:41 +0000

    Error uploading

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+* main - tests a function
+* @i: input number
+* Return: 0
+*/
+
+void positive_or_negative(int i);
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
