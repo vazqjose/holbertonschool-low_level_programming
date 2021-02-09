5f1db2cc1faf6d35905eecf280798362d9f38c19
vazqjose <vazqjose@gmail.com>
2021-02-09 16:40:22 +0000

    fixed N5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,15 +10,18 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		return ('+');
+		_putchar ('+');
+		return (1);
 	}
 	else if (n == 0)
 	{
-		return ('0');
+		_putchar('0');
+		return (0);
 	}
 	else if (n < 0)
 	{
-		return ('-');
+		_putchar('-');
+		return (-1);
 	}
 
 	return (0);
