8e834771484f6e230cc30faba19ba68d9f2631b9
vazqjose <vazqjose@gmail.com>
2021-02-10 01:39:46 +0000

    Fix typo

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,5 +1,5 @@
-#include "holberton.h"
 #include <stdio.h>
+#include "holberton.h"
 
 /**
  * largest_number - returns the largest of 3 numbers
@@ -16,7 +16,7 @@ int largest_number(int a, int b, int c)
 	if (a >= b && a >= c)
 	{
 		largest = a;
-	{
+	}
 	else if (b >= a && b >= c)
 	{
 		largest = b;
