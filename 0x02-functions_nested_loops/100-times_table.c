4d69748bf2e5757c44dd31dd6991b022072ae143
vazqjose <vazqjose@gmail.com>
2021-02-10 20:49:48 +0000

    Reduced lines

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,7 +16,6 @@ void print_times_table(int n)
 			for (j = 0; j <= n; j++)
 			{
 				result = i * j;
-
 				if (j == 0)
 				{
 					_putchar(result + '0');
@@ -42,13 +41,9 @@ void print_times_table(int n)
 					_putchar((result % 100) / 10  + '0');
 					_putchar(result / 10 + '0');
 				}
-
 				if (n == j)
-				{
 					_putchar(',');
-				}
 			}
-
 			_putchar('\n');
 		}
 	}
