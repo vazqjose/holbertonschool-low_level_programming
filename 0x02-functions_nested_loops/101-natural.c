ebb6b750f715b9849e7a65d54b40b7a878c1c1ac
vazqjose <vazqjose@gmail.com>
2021-02-10 03:54:24 +0000

    initial code added

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "holberton.h";
+
+/**
+ * main - Prints the sum of all multiples of 3 and 5
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	_putchar('\n');
+	return (0);
+}
