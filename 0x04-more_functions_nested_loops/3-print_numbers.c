97dbe9585940cd588ffcfe63330ce1336767acd7
vazqjose <vazqjose@gmail.com>
2021-02-11 22:32:36 +0000

    task 3 sent

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+* print_numbers - prints numers from 0 to 9, followed by a new line
+*
+* Return: Nothing
+*/
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+		_putchar('\n');
+	}
+}
