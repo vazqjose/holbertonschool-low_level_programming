0b3bf90d96ce3524ba9b8b9122bea6a2f2fe75cd
vazqjose <vazqjose@gmail.com>
2021-02-11 22:52:13 +0000

    Commited task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * print_most_numbers - prints most numbers except for 2 and 4
+ *
+ * Returns: Nothing
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 || i != 4)
+		{
+			_putchar(i + '0');
+		}
+	}
+_putchar('\n');
+
+}
