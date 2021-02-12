ba96c3e18c9c00474f28ad233dc2c01c32c8decc
vazqjose <vazqjose@gmail.com>
2021-02-12 03:01:04 +0000

    addded task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * print_square - function that prints a square, followed by a new line
+ * * @size: size of square
+ * * Return: void
+*/
+
+void print_square(int size)
+{
+	int c;
+	int i;
+
+	if (size > 0)
+	{
+		for (c = 0; c < size; c++)
+		{
+			for (i = 0; i < size; i++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
