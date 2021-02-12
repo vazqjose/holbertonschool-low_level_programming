137e0002bfa80964bd1986a6c3a1e02b14bde8bc
vazqjose <vazqjose@gmail.com>
2021-02-12 00:03:19 +0000

    hot fix on n4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,14 +9,14 @@ void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 48; i < 58; i++)
 	{
-		if (i != 50  || i != 52 )
+		if (i != 50 && i != 52 )
 		{
-			_putchar(i + '0');
+			_putchar(i);
 		}
 	}
 
-_putchar('\n');
+	_putchar('\n');
 
 }
