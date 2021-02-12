15ffecc4caaf17397da90d84b8b3dfd44376c89b
vazqjose <vazqjose@gmail.com>
2021-02-12 01:21:49 +0000

    Code fixed

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * more_numbers - Prints 10 times numbers from 0 to 14
+ *
+ * Return: No return
+ */
+
+
+void more_numbers(void)
+{
+	int row;
+	int col;
+
+	for (row = 0; row < 11; row++)
+	{
+		for (col = 0; col < 14; col++)
+		{
+			if (col > 9)
+			{
+				_putchar(col / 10 + '0');
+			}
+
+			_putchar(col % 10 + '0');
+		}
+
+		_putchar('\n');
+	}
+}
