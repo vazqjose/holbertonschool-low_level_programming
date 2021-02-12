4c1a3cb839e43c3fb0be4711a0b7215952622b27
vazqjose <vazqjose@gmail.com>
2021-02-12 03:03:52 +0000

    Added n9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main - Fizz Buzz interview question help 99.5% candidates
+*
+* Return: void
+*
+*/
+
+int main(void)
+{
+	int j;
+
+	for (j = 1; j <= 100; j++)
+	{
+		if (j % 3 == 0 && j % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (j % 3 == 0 && j % 5 != 0)
+		{
+			printf("Fizz");
+		}
+		else if (j % 5 == 0 && j % 3 != 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", j);
+		}
+		if (j != 100)
+			printf(" ");
+	}
+
+	printf("\n");
+
+	return (0);
+}
