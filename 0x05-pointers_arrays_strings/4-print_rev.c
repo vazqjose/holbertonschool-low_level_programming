514aa392676cc5f46fe7e10007552c06955415db
vazqjose <vazqjose@gmail.com>
2021-02-15 19:19:08 +0000

    Quick fix for betty

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,7 +11,11 @@ void print_rev(char *s)
 {
 	int i, len;
 
-	for (len = 0; s[len] != '\0'; len++);
+	for (len = 0; s[len] != '\0'; len++)
+	{
+
+	}
+
 	for (i = len - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
