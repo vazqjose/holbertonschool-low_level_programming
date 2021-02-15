ddadf1e9eced82b764136e9ba4524b43a135272c
vazqjose <vazqjose@gmail.com>
2021-02-15 18:07:57 +0000

    N4 added

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/*
+ * print_rev - prints a string in reverse
+ *
+ * Return: Nothings
+ *
+ */
+
+void print_rev(char *s)
+{
+	int i, len;
+
+	for (len = 0; s[len] != '\0'; len++)
+	{
+	}
+
+	for (i = len; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+}
