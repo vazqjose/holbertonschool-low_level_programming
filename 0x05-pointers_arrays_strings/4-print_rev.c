c1c4a3ce8c03a2daabf314b4c00b07df6bcc1474
vazqjose <vazqjose@gmail.com>
2021-02-15 19:07:21 +0000

    quick fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,11 +11,9 @@ void print_rev(char *s)
 {
 	int i, len;
 
-	for (len = 0; s[len] != '\0'; len++)
-	{
-	}
+	for (len = 0; s[len] != '\0'; len++);
 
-	for (i = len; i >= 0; i--)
+	for (i = len-1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
