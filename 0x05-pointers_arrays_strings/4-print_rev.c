bc992f289e6ec5d70c35a3a24af33f5de9b41cb9
vazqjose <vazqjose@gmail.com>
2021-02-15 19:16:57 +0000

    Fixed bug on n4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 
-/*
+/**
  * print_rev - prints a string in reverse
- *
- * Return: Nothings
+ * @s: Input string to count
+ * Return: Nothing
  *
  */
 
@@ -12,8 +12,7 @@ void print_rev(char *s)
 	int i, len;
 
 	for (len = 0; s[len] != '\0'; len++);
-
-	for (i = len-1; i >= 0; i--)
+	for (i = len - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
