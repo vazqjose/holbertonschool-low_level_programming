3477180a6468e7390692592d44daa37b2efab47f
vazqjose <vazqjose@gmail.com>
2021-02-16 02:18:45 +0000

    Added n5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string to be reversed
+ * Return: No return
+ *
+ */
+
+void rev_string(char *s)
+{
+	int i, len;
+	char temp1, temp2;
+
+	for (len = 0; s[len] != '\0'; len++)
+	{
+
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		temp1 = s[i];
+		temp2 = s[len];
+
+		*s[i] = temp2;
+		*s[len] = temp1;
+
+		len--;
+	}
+}
