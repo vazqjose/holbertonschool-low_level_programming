1fe8e5f58fe2a9d7ae4a870401f05d35c291a917
vazqjose <vazqjose@gmail.com>
2021-02-17 01:26:12 +0000

    Task 9 added

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _strcpy - copies a string to another buffer
+ * @dest: destination buffer
+ * @src: string input
+ *
+ * Return: pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
