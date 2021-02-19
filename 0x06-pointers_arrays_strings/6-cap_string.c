a659fba2e356634c301926555b36c8bd18b247af
vazqjose <vazqjose@gmail.com>
2021-02-19 04:33:56 +0000

    Quick fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -19,13 +19,15 @@ char *cap_string(char *str)
 				str[i] = str[i] - 32;
 			}
 		}
-		if (str[i] == ' ')
+
+		if (str[i] == '\t' || str[i] == '\n' || str[i] == ' ')
 		{
 			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 			{
 				str[i + 1] = str[i + 1] - 32;
 			}
 		}
+
 	}
 
 	return (str);
