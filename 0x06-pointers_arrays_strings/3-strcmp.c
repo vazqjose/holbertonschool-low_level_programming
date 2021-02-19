a01de869eb50baa76225081b56ace91100e9049a
vazqjose <vazqjose@gmail.com>
2021-02-19 01:33:35 +0000

    Added task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _strcmp - compares two string
+ * @s1: first string
+ * @s2: 2nd string
+ * Return: Reversed string
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	}
+	return (0);
+}
