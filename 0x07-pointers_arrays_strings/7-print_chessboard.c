f3f3601d97069ed650a7fe53655376b7faef0575
vazqjose <vazqjose@gmail.com>
2021-02-23 15:30:11 +0000

    Fix breakline issue

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -11,9 +11,9 @@ void print_chessboard(char (*a)[8])
 
 for (row = 0; row < 8; row++)
 {
-	for (col = 0; col < 8; col++)
+	for (col = 0; col <= 8; col++)
 	{
-		if (row == 8)
+		if (col == 8)
 		{
 			_putchar('\n');
 		}
