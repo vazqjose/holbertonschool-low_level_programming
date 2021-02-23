5de572df5058e590a4db46986f383a0bb39aafab
vazqjose <vazqjose@gmail.com>
2021-02-23 15:26:21 +0000

    Fixed common errors

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -7,17 +7,19 @@
 
 void print_chessboard(char (*a)[8])
 {
+	int row, col;
+
 for (row = 0; row < 8; row++)
 {
 	for (col = 0; col < 8; col++)
 	{
 		if (row == 8)
 		{
-			putchar('\n');
+			_putchar('\n');
 		}
 		else
 		{
-			putchar(a[row][col]);
+			_putchar(a[row][col]);
 		}
 	}
 }
