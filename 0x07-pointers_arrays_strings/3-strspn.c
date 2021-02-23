ead29a50df5c72e893b8faf4f35fd09a0d8da495
vazqjose <vazqjose@gmail.com>
2021-02-23 00:44:14 +0000

    Upload t3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -4,9 +4,24 @@
  * _strspn - Get the length of a prefix substring
  * @s: number of bytes to get
  * @accept: string to get bytes from
+ * Return: Number of bytes from accept in s
  */
 
 unsigned int _strspn(char *s, char *accept)
 {
+	int i, j;
+	int n = 0;
 
+	for (i = 0; accept[i] != '\0'; i++)
+	{
+		for (j = 0; s[j] != '\0'; i++)
+		{
+			if (accept[i] == s[j])
+			{
+				n++;
+				break;
+			}
+		}
+	}
+	return (0);
 }
