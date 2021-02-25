31fc6713e454dbc8d4763fe1c8733268f873425e
vazqjose <vazqjose@gmail.com>
2021-02-25 01:05:10 +0000

    tasks 1 and 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * _strlen_recursion - returns length of string
+ * @s: provided string
+ * Return: Lenght of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+		return(_strlen_recursion(s + 1) + 1);
+}
