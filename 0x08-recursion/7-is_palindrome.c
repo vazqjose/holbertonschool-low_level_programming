5676bed8d16142c682999bca71ff236ef1689976
vazqjose <vazqjose@gmail.com>
2021-02-26 03:26:10 +0000

    initial commit for t7

---
+++ 0x08-recursion/7-is_palindrome.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * is_palindrome - returns 1 if a string is palindrome and 0 if not
+ * @s: string inputted
+ * Return: 1 or 0
+ */
+
+int is_palindrome(char *s)
+{
+
+	if (s == '\0')
+	{
+		return (1);
+	}
+
+	return (0);
+}
