ea998cb1acbcab5ef3b6640c21480ec2509dc860
vazqjose <vazqjose@gmail.com>
2021-02-27 23:05:25 +0000

    t0 sent

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - function that prints its name
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: function name
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
