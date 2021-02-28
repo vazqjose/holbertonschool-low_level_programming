6d210827f6989650b56df33624a84365e8df3cec
vazqjose <vazqjose@gmail.com>
2021-02-28 02:01:01 +0000

    t4 added

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ * main - function that adds positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: numbers
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i;
+
+	/**
+	 * sum 56 9 6 2
+	 * argc = 5
+	 * argv[0] = sum
+	 */
+
+	if (argc == 1)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		if (!isdigit(*argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		sum += atoi(argv[i]);
+	}
+
+		printf("%d\n", sum);
+
+	return (0);
+}
