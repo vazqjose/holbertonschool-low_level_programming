1410f20565638085feb79d5499d3e3538a612fd3
vazqjose <vazqjose@gmail.com>
2021-03-03 02:06:41 +0000

    betty fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,43 +13,30 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *newstr;
-	int i, j, n1, n2, newsize;
+	int i, j, n1, n2, newsize, s;
 
-	for (i = 0; s1[i] != '\0'; i++)
-	{
-		/* get size of string1 */
-	}
+	for (i = 0; s1[i] != '\0'; i++) /* get size of string1 */
 
-	for (j = 0; s2[j] != '\0'; j++)
-	{
-		/* get size of string2 */
-	}
+	for (j = 0; s2[j] != '\0'; j++) /* get size of string2 */
 
 	newstr = malloc((sizeof(*s1) * i) + (sizeof(*s2) * j) + 1);
 
 	if (newstr == NULL)
-	{
-		return (NULL);
-	}
+	{ return (NULL); }
 	else
 	{
 		newsize = i + j + 1;
-
-		/* i = total string1 */
 		for (n1 = 0; n1 < i; n1++)
 		{
 			newstr[n1] = s1[n1];
 		}
 
-		/* j = total string 2 */
-		int s = 0;
+		s = 0;
 		for (n2 = n1; n2 < newsize; n2++)
 		{
 			newstr[n2] = s2[s];
 			s++;
 		}
-
 		return (newstr);
-
 	}
 }
