0c586e612011feb62867d6997d280102fc177714
vazqjose <vazqjose@gmail.com>
2021-03-04 03:42:09 +0000

    initial git

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * strtow - splits a string into words
+ * @str: input string
+ * Return: Null if str is null
+ */
+
+ar **strtow(char *str)
+{
+	int i;
+
+	if (str == NULL || str == "")
+		return (NULL);
+}
