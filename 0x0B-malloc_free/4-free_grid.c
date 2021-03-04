099ca117bb4d04af683399d024116619e6b8c85a
vazqjose <vazqjose@gmail.com>
2021-03-04 02:54:46 +0000

    initial t4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - frees a 2d grid
+ * @grid: input grid
+ * @height: height of grid
+ * Return: None
+ */
+
+void free_grid(int **grid, int height)
+{
+
+}
