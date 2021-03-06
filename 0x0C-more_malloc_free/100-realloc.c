e0b1a93773649a2f74d3a5f9bfdafc48a3bc4c4b
vazqjose <vazqjose@gmail.com>
2021-03-06 02:39:18 +0000

    placeholder t4

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _realloc - reallocates a memory block
+ * @ptr: provided pointer
+ * @old_size: size of provided ptr
+ * #new_size: new size in bytes of the new ptr
+ * Return: Null if zero
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *newPtr;
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (ptr != NULL)
+	{
+		free(ptr);
+		newPtr = malloc(sizeof(char) * new_size);
+		return (newPtr);
+	}
+}
