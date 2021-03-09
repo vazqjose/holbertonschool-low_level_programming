43ab1e48d3141c85fdfb96fa92ffeedad2b7f8ed
vazqjose <vazqjose@gmail.com>
2021-03-09 03:44:52 +0000

    Task0 upload

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - store dog info
+ * @name: name of dog
+ * @age: of dog
+ * @owner: name of dog owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
