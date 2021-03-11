54a7205e88a4616b5c96197848d4c7331df36a7f
vazqjose <vazqjose@gmail.com>
2021-03-11 00:19:37 +0000

    fix t2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,14 +15,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 
 	if (array == NULL || cmp == NULL)
-		return (0);
+		return (-1);
 
 	if (size <= 0)
 		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
-		if (cmp(array[i]))
+		if (cmp(array[i]) != 0)
 		{
 			return (i);
 		}
