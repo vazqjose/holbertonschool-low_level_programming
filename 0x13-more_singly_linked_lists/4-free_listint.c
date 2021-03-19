e2c2d2adf16a097dc40f5393514673f7c4b6e2b4
vazqjose <vazqjose@gmail.com>
2021-03-19 22:58:31 +0000

    t4 added

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ *free_listint - this will free the nodes
+ *@head: will point to the beginning of the nodes
+ *Return: Nothing
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
