2ddd3766d598dafb2f02e74b06a6a1e317a76272
vazqjose <vazqjose@gmail.com>
2021-03-19 04:47:21 +0000

    t2 added

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,13 +1,30 @@
 #include "lists.h"
 
 /**
+ * _strlen - returns the length of a string
+ * @s: string var
+ * Return:  Length of string
+ */
+
+int _strlen(const char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+
+	return (i);
+}
+
+/**
  * add_node - adds a new node at the begging of a list
  * @head: head of the link list
  * @str: string of the link list
  * Return: the address of the new element, or NULL if it fails
  */
 
- list_t *add_node(list_t head, const char *str)
+list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
 
@@ -24,7 +41,7 @@
 		return (NULL);
 
 	new_node->str = strdup(str);
-	new_node->len = str_len(str);
+	new_node->len = _strlen(str);
 
 	if (head == NULL)
 		new_node->next = NULL;
