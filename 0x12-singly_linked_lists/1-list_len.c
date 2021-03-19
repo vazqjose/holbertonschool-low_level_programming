a7fc1586966486c0a60aa3adae8a13e2c93188c7
vazqjose <vazqjose@gmail.com>
2021-03-19 03:58:52 +0000

    t1 fix

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,21 +1,23 @@
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a list_t list
+ * list_len - returns number of elemtns
  * @h: input list
  * Return: Number of nodes
  */
 
-size_t print_list(const list_t *h)
+
+size_t list_len(const list_t *h)
 {
 	size_t i = 0;
 
+	if (h->str == NULL)
+	{
+		return (0);
+	}
+
 	while (h != NULL)
 	{
-		if (h->str == NULL)
-		{
-			printf("[0] (nil)\n");
-		}
 		h = h->next;
 		i++;
 	}
