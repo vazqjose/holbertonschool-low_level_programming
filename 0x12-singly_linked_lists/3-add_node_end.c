b8477be2769e3620567db9edc56ca475123aedb7
vazqjose <vazqjose@gmail.com>
2021-03-19 05:14:17 +0000

    t3 added

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -53,8 +53,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		{
 			last_node = last_node->next;
 		}
+		last_node->next = new_node;
 	}
-	last_node->next = new_node;
-
 	return (new_node);
 }
