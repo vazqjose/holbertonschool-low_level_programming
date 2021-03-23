83ea0062720844953f0858bbe9c104b6a133d2c4
vazqjose <vazqjose@gmail.com>
2021-03-23 02:54:09 +0000

    changed logic

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,6 +23,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	newNode->n = n;
 
+	if (idx == 0)
+		return (NULL);
+
 	tmp = *head;
 
 	while (*head != NULL && i < idx - 1)
@@ -31,15 +34,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		tmp = tmp->next;
 	}
 
-	if (i == idx - 1)
-	{
-
 		newNode->next = tmp->next;
 		tmp->next = newNode;
 		return (newNode);
-	}
-	else
-	{
-		return (NULL);
-	}
 }
