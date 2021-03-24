135a6642d922246b5eb77bbc1457b652911f11a4
vazqjose <vazqjose@gmail.com>
2021-03-24 02:22:53 +0000

    Memory leak issue solved

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -9,7 +9,6 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *current = *head;
-	listint_t *previous = *head;
 	unsigned int i = 0;
 
 	if (*head == NULL)
@@ -22,7 +21,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (1);
 	}
 
-	while (current != NULL && i < index - 1)
+	while (current != NULL && i < index)
 	{
 		current = current->next;
 		i++;
@@ -30,8 +29,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	if (current == NULL || current->next == NULL)
 		return (-1);
 
-	previous->next = current->next;
-	free(current);
+	free(current->next);
+	current->next = current->next->next;
 
 	return (1);
 }
