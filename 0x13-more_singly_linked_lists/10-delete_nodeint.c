d0cc42013cd2d31d4127f94353aac9c5194face5
vazqjose <vazqjose@gmail.com>
2021-03-24 02:44:11 +0000

    get those last checks

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -8,12 +8,13 @@
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *current = *head;
+	listint_t *current;
 	unsigned int i = 0;
 
 	if (*head == NULL)
 		return (-1);
 
+	current = *head;
 	if (index == 0)
 	{
 		*head = current->next;
