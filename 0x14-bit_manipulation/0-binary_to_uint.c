4025ce6e7bbe2f330c17615dd14077b9dd0d4a8f
vazqjose <vazqjose@gmail.com>
2021-03-26 14:18:30 +0000

    t0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,5 +7,22 @@
 
 unsigned int binary_to_uint(const char *b)
 {
+	int i;
 
+	if (b == NULL)
+		return (0);
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] == '0' || b[i] == '1')
+		{
+
+		}
+		else
+		{
+			return (0);
+		}
+	}
+
+	return (0);
 }
