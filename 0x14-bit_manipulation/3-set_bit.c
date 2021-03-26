da1df7020a3242d95f5dd458bfd139dca22df070
vazqjose <vazqjose@gmail.com>
2021-03-26 19:17:04 +0000

    t3 added

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -8,5 +8,14 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned long int mask = 1;
 
+	if (index > 32)
+		return (-1);
+
+	mask = mask << index;
+
+	*n = *n | mask;
+
+	return (1);
 }
