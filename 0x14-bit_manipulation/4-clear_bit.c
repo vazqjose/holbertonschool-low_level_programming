5152a7211ab0e1bb6cd6d18f554629878387a02d
vazqjose <vazqjose@gmail.com>
2021-03-26 19:51:58 +0000

    t4 added

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -6,7 +6,16 @@
  * Return: 1 if it worked, or -1 if an error occurred
  */
 
-int clear_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index)
 {
-	
+	unsigned long int mask = 1;
+
+	if (index > 32)
+		return (-1);
+
+	mask = mask << index;
+
+	*n = *n & ~mask;
+
+	return (1);
 }
