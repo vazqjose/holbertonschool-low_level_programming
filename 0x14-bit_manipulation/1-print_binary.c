4390e6c356cfa581d1929c09cfae135eb17864ca
vazqjose <vazqjose@gmail.com>
2021-03-26 03:59:21 +0000

    t1 added

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,5 +7,33 @@
 
 void print_binary(unsigned long int n)
 {
+	unsigned long int newN, mask = 1;
+	int lenWidth = 0;
 
+	newN = n;
+	while (newN > 0)
+	{
+		lenWidth++;
+		newN >>= 1;
+	}
+
+	lenWidth -= 1;
+
+	if (lenWidth > 0)
+	{
+		mask = mask << lenWidth;
+	}
+
+	while (mask > 0)
+	{
+		if (n & mask)
+		{
+			_putchar('1');
+		}
+			else
+		{
+			_putchar('0');
+		}
+		mask >>= 1;
+	}
 }
