bdd9d2d487070aafce2396e1508c0951330adcc5
vazqjose <vazqjose@gmail.com>
2021-03-26 21:52:44 +0000

    t5 added

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -8,5 +8,19 @@
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
+	unsigned long int flipCount = 0;
+	unsigned long int number1 = n;
+	unsigned long int number2 = m;
+	unsigned long int result = 0;
 
+	result = number1 ^ number2;
+
+	while (result != 0)
+	{
+		if (result & 1)
+			flipCount++;
+
+		result = result >> 1;
+	}
+	return (flipCount);
 }
