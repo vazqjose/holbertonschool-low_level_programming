d1cf132ea513eea2bddc77050cb0a5eda2476cfd
vazqjose <vazqjose@gmail.com>
2021-03-30 23:50:53 -0400

    Create 1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,30 @@
+/*
+ * create_file - will create a file with write flag or truncate if got something
+ * @filename: will the be the file to create
+ * @text_content: will be the content for the file
+ * Return: will return 1 if sucess or -1 if fails
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, _write;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	_write = write(fd, text_content, _strlen(text_content));
+
+	if (_write == -1)
+		return (-1);
+
+	close(fd);
+	return (1);
+}
