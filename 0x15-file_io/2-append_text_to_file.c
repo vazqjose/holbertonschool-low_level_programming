6d5185fc1e9aec2fb8376b7fe079d10a2c13ec3c
vazqjose <vazqjose@gmail.com>
2021-03-31 20:41:36 +0000

    t2 fix

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,12 +14,10 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_APPEND | O_WRONLY | O_TRUNC);
+	fd = open(filename, O_APPEND | O_WRONLY);
 
 	if (fd == -1)
 		return (-1);
-	else
-		return (1);
 
 	if (text_content != NULL)
 	{
