31491132b0dad19389079d0305ed592fd37088e7
Jose Vazquez <vazqjose@gmail.com>
2021-05-01 01:48:26 +0000

    betty fix

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,7 +1,8 @@
 #include "lists.h"
-/*
+/**
  * print_dlistint - prints all elements of list
- * @dlistint_t: linked list to print
+ * @h: pointer to linked list header
+ * Return: Number of elements
  */
 size_t print_dlistint(const dlistint_t *h)
 {
