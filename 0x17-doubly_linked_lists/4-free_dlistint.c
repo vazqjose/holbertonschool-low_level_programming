9d2a6d089c96e5a85608efb21a11ecc3037e9b93
Jose Vazquez <vazqjose@gmail.com>
2021-05-01 18:36:28 +0000

    t4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,22 @@
+#import "lists.h"
+
+/**
+ * free_dlistint - frees a list
+ * @head: pointer to start
+ * Return: EXIT_SUCCESS
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	listint_t *tmp;
+
+	if (head == NULL)
+		return;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
