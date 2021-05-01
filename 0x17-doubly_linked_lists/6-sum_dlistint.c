9ad1ea0f37431f8838139b04545a1e5bf9bbb6a7
Jose Vazquez <vazqjose@gmail.com>
2021-05-01 17:11:49 +0000

    t6

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * sum_dlistint - sum all elements of list
+ * @head: pointer to linked list header
+ * Return: total sum 
+ * */
+int sum_dlistint(dlistint_t *head)
+{
+	size_t i = 0;
+
+	if (head == NULL)
+		return (0);
+
+	while (h != NULL)
+	{
+		h = h->next;
+		i += n;
+	}
+	return (i);
+}
