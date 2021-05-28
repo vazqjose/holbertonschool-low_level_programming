d2731290fb3fbe1f4c2925afbff007496a2d8327
Jose Vazquez <vazqjose@gmail.com>
2021-05-28 05:34:20 +0000

    t0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,13 +16,25 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
+	{
+		free(ht);
 		return (NULL);
+	}
 
 	ht->size = size;
-	ht->array = calloc(ht->size, sizeof(hash_node_t));
+
+	ht->array = calloc(size, sizeof(hash_node_t));
+	if (ht->array == NULL)
+	{
+		free(ht->array);
+		return (NULL);
+	}
 
 	for (i = 0; i < size; i++)
 		ht->array[i] = NULL;
+
+	free(ht);
+	free(ht->array);
 
 	return (ht);
 }
